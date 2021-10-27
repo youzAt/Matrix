15fd081e1de16d61845e217870dc3f03f16d7b84
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2021-10-27 05:37:27 +0330

    add matrix struct header file

---
+++ include/matrix_struct.hpp
@@ -0,0 +1,16 @@
+#ifndef MATRIX_STRUCT_HPP
+#define MATRIX_STRUCT_HPP
+#include<string>
+
+struct matrixS
+{
+	std::string name = "";
+	int size = 0;
+	int data_type;
+	float** mat_n = nullptr;
+	char** mat_c = nullptr;
+	std::string** mat_s = nullptr;
+};
+
+
+#endif // MATRIX_STRUCT_HPP
